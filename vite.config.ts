4b7cc23dff9c32963a21521c1951ede621d97df2
Debasish <relaxed.dhinta@gmil.com>
2024-12-26 19:52:39 +0530

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 80,
+  },
+  envPrefix: 'MLD_',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@common': path.resolve(__dirname, './src/common'),
+      '@config': path.resolve(__dirname, './src/configs'),
+    },
+  },
+});
